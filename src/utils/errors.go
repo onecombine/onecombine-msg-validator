9f851fb4307a9736a5f86a76f04d1f763bcf3ee4
Prawit Chaivong <prawit.c@zeabix.com>
2025-07-24 15:10:02 +0700

    Add partial cancel/refund not support error

--- src/utils/errors.go
+++ src/utils/errors.go
@@ -11,10 +11,12 @@ const (
 	CODE_ORDER_REF_EXIST   = "00400009"
 
 	// Reversal
-	CODE_REFUND_NOT_ALLOW         = "20402002"
-	CODE_CANCEL_NOT_ALLOW         = "20402003"
-	CODE_CURRENCY_NOT_MATCH_ORGTX = "20402005"
-	CODE_AMOUNT_EXCEEDED_ORGTX    = "20402006"
+	CODE_REFUND_NOT_ALLOW             = "20402002"
+	CODE_CANCEL_NOT_ALLOW             = "20402003"
+	CODE_CURRENCY_NOT_MATCH_ORGTX     = "20402005"
+	CODE_AMOUNT_EXCEEDED_ORGTX        = "20402006"
+	CODE_PARTIAL_REFUND_NOT_SUPPORTED = "20404004"
+	CODE_PARTIAL_CANCEL_NOT_SUPPORTED = "20404005"
 )
 
 const (
@@ -26,23 +28,28 @@ const (
 	MSG_ORDER_REF_EXIST   = "order_ref already exists"
 
 	// Reversal
-	MSG_REFUND_NOT_ALLOW         = "Refund transaction operation is not allow"
-	MSG_CANCEL_NOT_ALLOW         = "Cancel transaction operation is not allowed"
-	MSG_CURRENCY_NOT_MATCH_ORGTX = "Refund/Cancel currency does not match transaction currency"
-	MSG_AMOUNT_EXCEEDED_ORGTX    = "Refund/Cancel amount exceeds remaining amount that can be refunded/cancelled"
+	MSG_REFUND_NOT_ALLOW             = "Refund transaction operation is not allow"
+	MSG_CANCEL_NOT_ALLOW             = "Cancel transaction operation is not allowed"
+	MSG_CURRENCY_NOT_MATCH_ORGTX     = "Refund/Cancel currency does not match transaction currency"
+	MSG_AMOUNT_EXCEEDED_ORGTX        = "Refund/Cancel amount exceeds remaining amount that can be refunded/cancelled"
+	MSG_PARTIAL_REFUND_NOT_SUPPORTED = "Partial refund is not supported"
+	MSG_PARTIAL_CANCEL_NOT_SUPPORTED = "Partial cancel is not supported"
 )
 
 var (
 	errorDict = map[string]string{
-		CODE_INTERNAL_ERROR:           MSG_INTERNAL_ERROR,
-		CODE_APIKEY_MISSING:           MSG_APIKEY_MISSING,
-		CODE_INVALID_SIGNATURE:        MSG_INVALID_SIGNATURE,
-		CODE_BAD_REQUEST:              MSG_BAD_REQUEST,
-		CODE_ORDER_NOT_FOUND:          MSG_ORDER_NOT_FOUND,
-		CODE_REFUND_NOT_ALLOW:         MSG_REFUND_NOT_ALLOW,
-		CODE_CANCEL_NOT_ALLOW:         MSG_CANCEL_NOT_ALLOW,
-		CODE_CURRENCY_NOT_MATCH_ORGTX: MSG_CURRENCY_NOT_MATCH_ORGTX,
-		CODE_AMOUNT_EXCEEDED_ORGTX:    MSG_AMOUNT_EXCEEDED_ORGTX,
+		CODE_INTERNAL_ERROR:               MSG_INTERNAL_ERROR,
+		CODE_APIKEY_MISSING:               MSG_APIKEY_MISSING,
+		CODE_INVALID_SIGNATURE:            MSG_INVALID_SIGNATURE,
+		CODE_BAD_REQUEST:                  MSG_BAD_REQUEST,
+		CODE_ORDER_NOT_FOUND:              MSG_ORDER_NOT_FOUND,
+		CODE_ORDER_REF_EXIST:              MSG_ORDER_REF_EXIST,
+		CODE_REFUND_NOT_ALLOW:             MSG_REFUND_NOT_ALLOW,
+		CODE_CANCEL_NOT_ALLOW:             MSG_CANCEL_NOT_ALLOW,
+		CODE_CURRENCY_NOT_MATCH_ORGTX:     MSG_CURRENCY_NOT_MATCH_ORGTX,
+		CODE_AMOUNT_EXCEEDED_ORGTX:        MSG_AMOUNT_EXCEEDED_ORGTX,
+		CODE_PARTIAL_REFUND_NOT_SUPPORTED: MSG_PARTIAL_REFUND_NOT_SUPPORTED,
+		CODE_PARTIAL_CANCEL_NOT_SUPPORTED: MSG_PARTIAL_CANCEL_NOT_SUPPORTED,
 	}
 )
 
